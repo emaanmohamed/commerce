e949ac20716d1ba4fd984a2b35dc28d390005031
emaanmohamed <emaan.mohamed.ka@gmail.com>
2025-04-10 15:31:02 +0200

    first init

---
+++ cmd/api/api.go
@@ -0,0 +1,33 @@
+package api
+
+import (
+	"database/sql"
+	"github.com/emaanmohamed/commerce/service/user"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+type APIServer struct {
+	addr string
+	db   *sql.DB
+}
+
+func NewAPIServer(addr string, db *sql.DB) *APIServer {
+	return &APIServer{
+		addr: addr,
+		db:   db,
+	}
+}
+
+func (s *APIServer) Run() error {
+	router := mux.NewRouter()
+	subRouter := router.PathPrefix("/api/v1").Subrouter()
+
+	userHandler := user.NewHandler()
+	userHandler.RegisterRoutes(subRouter)
+
+	log.Println("Starting API server on", s.addr)
+
+	return http.ListenAndServe(s.addr, subRouter)
+}
---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/emaanmohamed/commerce
+
+go 1.21.0
+
+toolchain go1.23.0
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/go-sql-driver/mysql v1.9.1 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
